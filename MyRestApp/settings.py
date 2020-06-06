da2baff31e6c665e032a50fb9f1714828f7fd921
GAURAVCHIT <gaurav198.rr@gmail.com>
2020-06-06 16:09:12 +0530

    postgres connected to local host

--- MyRestApp/settings.py
+++ MyRestApp/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'MyRestApp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'newRestTest',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres1234',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
